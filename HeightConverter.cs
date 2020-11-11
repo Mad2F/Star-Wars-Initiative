4b8d4b94b4b1c0de880df48cf135ade36a98f07f
MadelineFleury <madeline.fleury@eleves.enpc.fr>
2020-11-11 18:26:49 +0100

    configuring gui height width to be fullscreen correctly

---
+++ HeightConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Star_Wars
+{
+    class HeightConverter: System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            Console.WriteLine(targetType + " " + value + " " + parameter);
+            return System.Convert.ToDouble(value) -
+                   System.Convert.ToDouble(parameter);
+        }
+
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PercentageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Star_Wars
+{
+    public class PercentageConverter : System.Windows.Data.IValueConverter
+    {
+        public object Convert(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            Console.WriteLine(targetType + " " + value + " " + parameter);
+            return System.Convert.ToDouble(value) *
+                   System.Convert.ToDouble(parameter) / 100;
+        }
+
+        public object ConvertBack(object value,
+            Type targetType,
+            object parameter,
+            System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
